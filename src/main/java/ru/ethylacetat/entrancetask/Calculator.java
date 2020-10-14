24243815b724de78b71fd04cebb2a4b1ec624246
ethylacetat <ethylacetat0@gmail.com>
2020-10-14 18:02:29 +0300

    Creating a universe

---
+++ src/main/java/ru/ethylacetat/entrancetask/Calculator.java
@@ -0,0 +1,8 @@
+package ru.ethylacetat.entrancetask;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/ru/ethylacetat/entrancetask/CalculatorTest.java
@@ -0,0 +1,12 @@
+package ru.ethylacetat.entrancetask;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void main() {
+    }
+}
